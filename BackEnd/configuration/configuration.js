105db321ba23020319e4fc17fe7f6230b2aa3be7
gregriedo <gregory.riedo@gmail.com>
2021-12-13 13:44:29 +0100

    création fichier configuration.js dans sous-dossier configuration

---
+++ BackEnd/configuration/configuration.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+module.exports ={
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DATABASE: process.env.DB_DATABASE, 
+  PORT: process.env.DB_PORT,
+
+};
