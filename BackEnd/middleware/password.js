83bacf1bbfd70c7268a6cdb5b1c8dcc10fd51697
gregriedo <gregory.riedo@gmail.com>
2021-12-27 16:39:09 +0100

    modification de password.js

--- BackEnd/middleware/password.js
+++ BackEnd/middleware/password.js
@@ -1,40 +1,25 @@
 const passwordValidator = require('password-validator');
 
-module.exports = (req, res, next) => {
-    try {
-        const schema = new passwordValidator();
-        schema
-.is().min(8)                                    
-.is().max(100)                                  
-.has().uppercase()                              
-.has().lowercase()                             
-.has().digits(2)                                
-.has().not().spaces()                           
-.is().not().oneOf(['Passw0rd', 'Password123']);
+        const passwordSchema = new passwordValidator();
+        
+        // Schema que doit respecter le mot de passe
+        passwordSchema
+          .is().min(5)                                    
+          .is().max(10)                                  
+          .has().uppercase()                              
+          .has().lowercase()                             
+          .has().digits(2)                                
+          .has().not().spaces()                           
+          .is().not().oneOf(['Passw0rd', 'Password123']);
 
-console.log(schema.validate('validPASS123'));
-// => true
-console.log(schema.validate('invalidPASS'));
-// => false
+      
 
-// Get a full list of rules which failed
-console.log(schema.validate('joke', { list: true }));
+   
 
-      if (req.body.password && req.body.password !== schema) {
-        throw 'Mot de Passe Incorrect !';
-      } else {
-        next();
-      }
-    } catch {
-      res.status(401).json({
-        error: new Error('Invalid request!')
-      });
-    }
-  };
-// Create a schema
-
-
-// Add properties to it
- // Blacklist these values
-
-// Validate against a password string
+     module.exports = (req, res, next) =>{
+        if(passwordSchema.validate(req.body.password)){
+          next();
+        }else{
+          return res.status(400).json({error:`Le Mot de Passe n'est pas assez Fort ${passwordSchema.validate('req.body.password', { list: true })}` }) 
+        }
+     }
