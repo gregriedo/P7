b6ddc0a4b56bf4409b8dd98a0e1fd319a4787a28
gregriedo <gregory.riedo@gmail.com>
2021-12-27 16:40:43 +0100

    ajout du password validator dans users.js dans sous dossier routes

--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth.js');
 const multer = require('../middleware/multer-config');
-const passwordValidator = require('../middleware/password');
+const password = require('../middleware/password');
+
 
 const userCtrl = require('../controllers/users');
 
-router.post('/signup', multer, passwordValidator, userCtrl.signup);
+router.post('/signup', multer, password, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 router.get('/', auth, userCtrl.getAllUsers);
@@ -14,4 +15,4 @@ router.get('/:id', auth, userCtrl.findOneUser);
 router.put('/:id', auth, multer, userCtrl.modifyUser);
 router.delete('/:id', auth, userCtrl.deleteUser);
 
-module.exports = router;
+module.exports = router; 
