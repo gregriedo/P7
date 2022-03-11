9b989a0b7e87aaed3daf80afbda550fe0ab1f243
gregriedo <gregory.riedo@gmail.com>
2022-03-11 11:57:55 +0100

    modifications dans multer-config.js

--- BackEnd/middleware/multer-config.js
+++ BackEnd/middleware/multer-config.js
@@ -1,5 +1,6 @@
 const multer = require('multer');
 
+
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
@@ -16,5 +17,6 @@ const storage = multer.diskStorage({
     callback(null, name + Date.now() + '.' + extension);
   }
 });
+
 
 module.exports = multer({storage: storage}).single('image');
