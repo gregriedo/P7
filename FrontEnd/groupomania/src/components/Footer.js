0463fb9cc09a9f96c86aa2ffdfaf58a60f5ca47f
gregriedo <gregory.riedo@gmail.com>
2022-03-14 21:16:53 +0100

    ajout fichier footer dans components dans le Front end

--- FrontEnd/groupomania/src/components/Footer.js
+++ FrontEnd/groupomania/src/components/Footer.js
@@ -1,9 +1,36 @@
 
-export default function Footer() {
-    
-   
-    
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import HomeIcon from '@mui/icons-material/Home';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
+import PersonIcon from '@mui/icons-material/Person';
+
+export default function LabelBottomNavigation() {
+
+
+  
+
   return (
-    <h1>En construction</h1>
+    <BottomNavigation sx={{ width: 500 }} >
+      <BottomNavigationAction
+        label="Home"
+        value="Home"
+        icon={<HomeIcon />}
+        href='../'
+      />
+      <BottomNavigationAction
+        label="User Page"
+        value="User Page"
+        icon={<PersonIcon />}
+        href='../userBoard'
+      />
+      <BottomNavigationAction
+        label="Contact"
+        value="Contact"
+        icon={<ContactMailIcon />}
+        href='mailto:groupomania@gmail.com'
+      />
+    </BottomNavigation>
   );
 }
