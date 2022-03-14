d4d512e38c7d47f9ee2dacca6b331554bb38ad02
gregriedo <gregory.riedo@gmail.com>
2022-03-14 21:21:22 +0100

    ajout function delete dans Feed.js

--- FrontEnd/groupomania/src/pages/feed.js
+++ FrontEnd/groupomania/src/pages/feed.js
@@ -10,31 +10,56 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddCommentIcon from '@mui/icons-material/AddComment';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 
 export default function Feed() {
     const user = JSON.parse(localStorage.getItem("user"));
   const token = user.token;
    const [posts, setPosts] = useState([]);
+   const navigate = useNavigate();
   
 
-  useEffect(() => {
-    axios.get("http://localhost:3000/api/articles/",{ headers: {"Authorization" : `Bearer ${token}`} })
-    .then((response) => {
-
-      setPosts(response.data);
-    });
+   useEffect(() =>{
+    const res = axios
+    .get('http://localhost:3000/api/articles/',{ headers: {"Authorization" : `Bearer ${token}`} })
+    res.then((data)=>{
+      setPosts(data.data);
+    })
   }, [token]);
+
+  const deletePost = (e) =>{
+    e.preventDefault();
+     
+     axios.delete(`http://localhost:3000/api/article/${posts.id}`,{
+       headers: {
+         'Content-Type': 'application/json',
+         'Accept': 'application/json',
+         'authorization': 'Bearer ' + user.token,
+       },
+     })
+     .then(() => {
+      navigate("../userBoard", { replace: true });
+      alert("Votre Article a bien été supprimé!")
+       window.location.reload();
+       })
+       .catch(function (error) {
+         alert("Vous n'avez pas les droits pour utiliser cette fonction!")
+         console.log(error);
+       });
+      }
+       
 
   return (
     <div className='feed'>
         <div className='feedWrapper'>
         {posts.map((p) =>(
-            <Card key={p.id}sx={{ maxWidth: 345 }}>
+            <Card key={p.id}sx={{ maxWidth: 500 }}>
             <CardHeader
               avatar={
                 <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -61,12 +86,16 @@ export default function Feed() {
               </Typography>
             </CardContent>
             <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon />
-              </IconButton>
-              <IconButton aria-label="share">
-                <ShareIcon />
-              </IconButton>
+            <IconButton href="/newComment" aria-label="reply">
+              <AddCommentIcon />
+            </IconButton>
+            <IconButton aria-label="reply">
+              <ThumbUpIcon />
+            </IconButton>
+            
+            <IconButton onClick={deletePost} aria-label="delete article">
+            <DeleteIcon />
+            </IconButton>
              
              
             </CardActions>
