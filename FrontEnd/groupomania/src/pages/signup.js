c056f21f62088eb00afbbd1f5b5938e58ee24997
gregriedo <gregory.riedo@gmail.com>
2022-04-12 14:34:59 +0200

    ajout possibilité de mettre une image profil lors du signup

--- FrontEnd/groupomania/src/pages/signup.js
+++ FrontEnd/groupomania/src/pages/signup.js
@@ -5,68 +5,34 @@ import InputLabel from '@mui/material/InputLabel';
 import IconButton from '@mui/material/IconButton';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import Button from '@mui/material/Button';
-import InputAdornment from '@mui/material/InputAdornment';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
-
-
+import '../styles/signup.css';
 
 
 export default function CreateAccount() {
     const navigate = useNavigate();
-    const [values, setValues] = React.useState({
-      showPassword: false,
-    });
-    const handleClickShowPassword = () => {
-      setValues({
-        ...values,
-        showPassword: !values.showPassword,
-      });
-    };
-  
-    const handleMouseDownPassword = (event) => {
-      event.preventDefault();
-    };
-    const [status, setStatus] = useState('')
-    
+    const [status, setStatus] = useState('')  
     const [mail, setMail] = useState();
     const [password, setPassword] = useState();
     const [username, setUsername] = useState();
-    const [imageProfil, setImageprofil] = useState({ preview: '', data: '' });
-    const [isAdmin] = useState(false);
-
-  
-    const handleFileChange = (e) => {
-      const img = {
-        preview: URL.createObjectURL(e.target.files[0]),
-        data: e.target.files[0],
-      }
-      setImageprofil(img)
-    }
-
-    const signup = (e) =>{
-      e.preventDefault();
-      let myForm = document.getElementById('myForm');
-      let formData = new FormData(myForm);
-     
+    const [imageProfil, setImageprofil] = useState();
+    const [isAdmin] = useState(0);
+   
+    const signup = async event =>{
+      event.preventDefault();
+       
+      let formData = new FormData();
       formData.append('mail', mail)
       formData.append('password', password)
       formData.append('username', username)
       formData.append('image', imageProfil)
       formData.append('isAdmin', isAdmin)
-      
-      console.log(formData);
-     
-    axios.post("http://localhost:3000/api/auth/signup",formData,{
+  
+    axios.post("http://localhost:3000/api/auth/signup", formData,{   
+      headers: { "Content-Type": "multipart/form-data" } 
+    })
        
-        headers: {
-          "Content-Type": "multipart/form-data",
-
-        }
-      })
         .then((response) => {
           setStatus(response.statusText)
            console.log(response);
@@ -82,9 +48,8 @@ export default function CreateAccount() {
         
           
   return (
-    
-      <form id="myForm" name="myForm" encType="multipart/form-data" onSubmit={signup}>
-      <div>
+    <div className="App">
+      <form id="myForm" name="myForm" encType="multipart/form-data" >
       <FormControl variant="standard">
         <InputLabel htmlFor="component-simple">Mail</InputLabel>
         <Input id="mail"
@@ -93,7 +58,6 @@ export default function CreateAccount() {
               placeholder=" Email"
               required pattern="[a-zâäàéèùêëîïôöçñA-Z0-9.-_]+[@]{1}[a-zA_Z0-9.-_]+[.]{1}[a-z]{2,4}" 
               title="Ex: groupomania@gmail.com, ..."
-              value={values.mail}
               onChange={(event) => {
                 setMail(event.target.value);
               }}
@@ -102,34 +66,18 @@ export default function CreateAccount() {
                 "aria-label": "mail",
              }} />
       </FormControl>
-    </div>
-    <div>
     <FormControl variant="standard">
         <InputLabel htmlFor="component-simple">Password</InputLabel>
         <Input   id="password"
-            type={values.showPassword ? "text" : "password"}
+            type="password"
             name="password"
             placeholder=" Password"
             title='Doit contenir min 2 chiffres'
-            value={values.password}
             onChange={(event) => {
               setPassword(event.target.value);
             }}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-              }
             />
       </FormControl>
-    </div>
-    <div>
     <FormControl variant="standard">
         <InputLabel htmlFor="component-simple">Username</InputLabel>
         <Input id="username"
@@ -138,7 +86,6 @@ export default function CreateAccount() {
               placeholder=" Username"
               required pattern="[a-zâäàéèùêëîïôöçñA-Z\s]{3,30}" 
               title="Ex: SuperMario, Jo ..."
-              value={values.username}
               onChange={(event) => {
                 setUsername(event.target.value);
                }}
@@ -147,25 +94,25 @@ export default function CreateAccount() {
                  "aria-label": " Username",
               }} />
       </FormControl>
-    </div>
-    <div>
-    {imageProfil.preview && <img src={imageProfil.preview} alt="UserPic" width='100' height='100' />}
+    
+       <label htmlFor="icon-button-file">
+        <Input accept="image/*" className="input" id="icon-button-file" type="file" name="image" filename={imageProfil} 
+          onChange={e => setImageprofil(e.target.files[0])} />
+          
+        <IconButton color="primary" aria-label="upload picture" component="span">
+          <PhotoCamera />
+        </IconButton>
+      </label>
       
-    <label htmlFor="icon-button-file">
-  <Input accept="image/*" id="icon-button-file" type="file" name="image" onChange={handleFileChange} />
-  <IconButton color="primary" aria-label="upload picture" component="span">
-    <PhotoCamera />
-  </IconButton>
-</label>
-    </div>
-    <Button variant="contained" type="Submit">Créer Votre Compte</Button>
-    {status && <h4>{status}</h4>}
-  </form>
-   
-    
   
+       <Button variant="contained" type="Submit" onClick={signup}>Créer Votre Compte</Button>
     
-  );
-};
+      </form>
+       
+    </div>
+      
+    
+  )
+}
 
 
